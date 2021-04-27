f9123ddb24950821e957e51f695056c0ffcbe7bb
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-27 16:25:35 -0600

    built out navigator with basic options

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Platform, Text } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator} from "react-navigation-drawer";
+import { Ionicons } from "@expo/vector-icons";
+
+import CartScreen from "../screens/CartScreen";
+import EditProductScreen from "../screens/EditProductScreen";
+import ManageProductsScreen from "../screens/ManageProductsScreen";
+import OrdersScreen from "../screens/OrdersScreen";
+import ProductDetailScreen from "../screens/ProductDetailScreen";
+import ShopScreen from "../screens/ShopScreen";
+import Colors from "../constants/Colors";
+
+const defaultStackNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+  },
+  headerTitleStyle: {
+    fontFamily: "zendots"
+  },
+  headerBackTitleStyle: {
+    fontFamily: "nunito"
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+};
+
+const ShopNavigator = createStackNavigator({
+  Shop: ShopScreen,
+  ProductDetail: ProductDetailScreen,
+  Cart: CartScreen
+}, {
+  defaultNavigationOptions: defaultStackNavOptions
+});
+
+const OrdersNavigator = createStackNavigator({
+  Orders: OrdersScreen
+}, {
+  defaultNavigationOptions: defaultStackNavOptions
+});
+
+const ManageProductsNavigator = createStackNavigator({
+  ManageProducts: ManageProductsScreen,
+  EditProduct: EditProductScreen
+}, {
+  defaultNavigationOptions: defaultStackNavOptions
+});
+
+const MainNavigator = createDrawerNavigator({
+  ShopNav: {
+    screen: ShopNavigator,
+    navigationOptions: {
+      drawerLabel: "Shop"
+    }
+  },
+  OrdersNav: {
+    screen: OrdersNavigator,
+    navigationOptions: {
+      drawerLabel: "My Orders"
+    }
+  },
+  ManageProductsNav: {
+    screen: ManageProductsNavigator,
+    navigationOptions: {
+      drawerLabel: "My Products"
+    }
+  }
+}, {
+  contentOptions: {
+    activeTintColor: Colors.accentColor,
+    labelStyle: {
+      fontFamily: "nunito"
+    }
+  }
+});
+
+export default createAppContainer(MainNavigator);
