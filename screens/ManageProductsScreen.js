59cb55053608c7877dc603919971857d5ee1de32
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-30 16:27:54 -0600

    added navigation buttons to header

--- screens/ManageProductsScreen.js
+++ screens/ManageProductsScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "../components/HeaderButton";
 
 const ManageProductsScreen = props => {
   return (
@@ -8,6 +11,34 @@ const ManageProductsScreen = props => {
     </View>
   );
 };
+
+ManageProductsScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "My Products",
+    headerLeft: () =>
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>,
+    headerRight: () =>
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add Product"
+          iconName="add-circle-outline"
+          onPress={() => {
+            navData.navigation.navigate({
+              routeName: "EditProduct"
+            });
+          }}
+        />
+      </HeaderButtons>
+  };
+}
 
 const styles = StyleSheet.create({
 
